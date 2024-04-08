abc2ee1ba591b50282523b7939aea02a26786293
shanye <shanye.xzq@alibaba-inc.com>
2024-04-08 15:40:32 +0800

    improve the examples for HBR

--- quickstarts/Hybrid_Backup_Recovery(HBR)/101-hbr-vault-docs-Example/main.tf
+++ quickstarts/Hybrid_Backup_Recovery(HBR)/101-hbr-vault-docs-Example/main.tf
@@ -1,3 +1,7 @@
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
 resource "alicloud_hbr_vault" "example" {
-  vault_name = "example_value"
+  vault_name = "example_value_${random_integer.default.result}"
 }

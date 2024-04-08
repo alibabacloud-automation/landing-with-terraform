c7c3d37db4bf266d9540ce8cb025304d42ed2edf
shanye <shanye.xzq@alibaba-inc.com>
2024-04-08 15:40:32 +0800

    improve the examples for HBR

--- quickstarts/Hybrid_Backup_Recovery(HBR)/101-hbr-nas-backup-plan-docs-Example/main.tf
+++ quickstarts/Hybrid_Backup_Recovery(HBR)/101-hbr-nas-backup-plan-docs-Example/main.tf
@@ -1,5 +1,10 @@
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
 resource "alicloud_hbr_vault" "default" {
-  vault_name = "terraform-example2"
+  vault_name = "terraform-example-${random_integer.default.result}"
 }
 
 resource "alicloud_nas_file_system" "default" {

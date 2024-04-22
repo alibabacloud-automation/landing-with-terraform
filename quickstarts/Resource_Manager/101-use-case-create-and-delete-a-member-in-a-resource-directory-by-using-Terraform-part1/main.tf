fe19f80cf803c914e287dfb9468ea0b880b27dac
shanye <shanye.xzq@alibaba-inc.com>
2024-04-22 11:38:32 +0800

    fix examples for KMS

--- quickstarts/Resource_Manager/101-use-case-create-and-delete-a-member-in-a-resource-directory-by-using-Terraform-part1/main.tf
+++ quickstarts/Resource_Manager/101-use-case-create-and-delete-a-member-in-a-resource-directory-by-using-Terraform-part1/main.tf
@@ -1,3 +1,7 @@
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
 resource "alicloud_resource_manager_account" "default" {
-  display_name = "RDAccount_auto"
+  display_name = "RDAccount_auto_${random_integer.default.result}"
 }

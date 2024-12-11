92311059a9d646befa2199d08f568e0c56f9ebb3
lsy <3096027587@qq.com>
2024-12-11 17:06:35 +0800

    201-use-case-create-ack-managed-cluster

--- quickstarts/Container_Service_for_Kubernetes(ACK)/201-use-case-create-ack-managed-cluster/main.tf
+++ quickstarts/Container_Service_for_Kubernetes(ACK)/201-use-case-create-ack-managed-cluster/main.tf
@@ -13,12 +13,6 @@ variable "cluster_spec" {
   default     = "ack.pro.small"
 }
 
-variable "ack_version" {
-  type        = string
-  description = "Desired Kubernetes version. "
-  default     = "1.28.9-aliyun.1"
-}
-
 # 指定虚拟交换机（vSwitches）的可用区。
 variable "availability_zone" {
   description = "The availability zones of vswitches."
@@ -32,7 +26,7 @@ variable "node_vswitch_ids" {
   default     = []
 }
 
-# 当没有提供node_vswitch_ids时，这个变量定义了用于创建新vSwitches的CIDR地址块列表。
+# 用于创建新vSwitches的CIDR地址块列表。
 variable "node_vswitch_cidrs" {
   description = "List of cidr blocks used to create several new vswitches when 'node_vswitch_ids' is not specified."
   type        = list(string)
@@ -59,7 +53,7 @@ variable "worker_instance_types" {
   default     = ["ecs.g6.2xlarge", "ecs.g6.xlarge"]
 }
 
-# 设置工作阶段的密码
+# 设置工作节点的密码。
 variable "password" {
   description = "The password of ECS instance."
   default     = "Test123456"
@@ -71,7 +65,6 @@ variable "cluster_addons" {
     name   = string
     config = string
   }))
-
   default = [
     {
       "name"   = "terway-eniip",
@@ -157,9 +150,8 @@ resource "alicloud_vswitch" "terway_vswitches" {
 
 # Kubernetes托管版。
 resource "alicloud_cs_managed_kubernetes" "default" {
-  name                         = local.k8s_name_terway # Kubernetes集群名称。
-  cluster_spec                 = var.cluster_spec      # 创建Pro版集群。
-  version                      = var.ack_version
+  name                         = local.k8s_name_terway                                         # Kubernetes集群名称。
+  cluster_spec                 = var.cluster_spec                                              # 创建Pro版集群。
   worker_vswitch_ids           = split(",", join(",", alicloud_vswitch.vswitches.*.id))        # 节点池所在的vSwitch。指定一个或多个vSwitch的ID，必须在availability_zone指定的区域中。
   pod_vswitch_ids              = split(",", join(",", alicloud_vswitch.terway_vswitches.*.id)) # Pod虚拟交换机。
   new_nat_gateway              = true                                                          # 是否在创建Kubernetes集群时创建新的NAT网关。默认为true。
@@ -167,8 +159,7 @@ resource "alicloud_cs_managed_kubernetes" "default" {
   slb_internet_enabled         = true                                                          # 是否为API Server创建Internet负载均衡。默认为false。
   enable_rrsa                  = true
   control_plane_log_components = ["apiserver", "kcm", "scheduler", "ccm"] # 控制平面日志。
-
-  dynamic "addons" { # 组件管理。
+  dynamic "addons" {                                                      # 组件管理。
     for_each = var.cluster_addons
     content {
       name   = lookup(addons.value, "name", var.cluster_addons)
@@ -184,15 +175,12 @@ resource "alicloud_cs_kubernetes_node_pool" "default" {
   vswitch_ids           = split(",", join(",", alicloud_vswitch.vswitches.*.id)) # 节点池所在的vSwitch。指定一个或多个vSwitch的ID，必须在availability_zone指定的区域中。
   instance_types        = var.worker_instance_types
   instance_charge_type  = "PostPaid"
-  runtime_name          = "containerd"
-  runtime_version       = "1.6.20"
   desired_size          = 2            # 节点池的期望节点数。
   password              = var.password # SSH登录集群节点的密码。
   install_cloud_monitor = true         # 是否为Kubernetes的节点安装云监控。
   system_disk_category  = "cloud_efficiency"
   system_disk_size      = 100
   image_type            = "AliyunLinux"
-
   data_disks {              # 节点数据盘配置。
     category = "cloud_essd" # 节点数据盘种类。
     size     = 120          # 节点数据盘大小。
@@ -205,23 +193,18 @@ resource "alicloud_cs_kubernetes_node_pool" "managed_node_pool" {
   node_pool_name = local.managed_nodepool_name                            # 节点池名称。
   vswitch_ids    = split(",", join(",", alicloud_vswitch.vswitches.*.id)) # 节点池所在的vSwitch。指定一个或多个vSwitch的ID，必须在availability_zone指定的区域中。
   desired_size   = 0                                                      # 节点池的期望节点数。
-
   management {
     auto_repair     = true
     auto_upgrade    = true
     max_unavailable = 1
   }
-
   instance_types        = var.worker_instance_types
   instance_charge_type  = "PostPaid"
-  runtime_name          = "containerd"
-  runtime_version       = "1.6.20"
   password              = var.password
   install_cloud_monitor = true
   system_disk_category  = "cloud_efficiency"
   system_disk_size      = 100
   image_type            = "AliyunLinux"
-
   data_disks {
     category = "cloud_essd"
     size     = 120
@@ -233,21 +216,16 @@ resource "alicloud_cs_kubernetes_node_pool" "autoscale_node_pool" {
   cluster_id     = alicloud_cs_managed_kubernetes.default.id
   node_pool_name = local.autoscale_nodepool_name
   vswitch_ids    = split(",", join(",", alicloud_vswitch.vswitches.*.id))
-
   scaling_config {
     min_size = 1
     max_size = 10
   }
-
   instance_types        = var.worker_instance_types
-  runtime_name          = "containerd"
-  runtime_version       = "1.6.20"
   password              = var.password # SSH登录集群节点的密码。
   install_cloud_monitor = true         # 是否为kubernetes的节点安装云监控。
   system_disk_category  = "cloud_efficiency"
   system_disk_size      = 100
   image_type            = "AliyunLinux3"
-
   data_disks {              # 节点数据盘配置。
     category = "cloud_essd" # 节点数据盘种类。
     size     = 120          # 节点数据盘大小。

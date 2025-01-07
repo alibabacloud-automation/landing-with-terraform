d3068b9576a490299f855167c67cce56e2df3b73
lsy <3096027587@qq.com>
2025-01-07 15:03:30 +0800

    201-use-case-cdn-accelerate-resource-from-ecs

---
+++ quickstarts/CDN/201-use-case-cdn-accelerate-resource-from-ecs/README.md
@@ -0,0 +1,55 @@
+## Introduction
+
+<!-- DOCS_DESCRIPTION_CN -->
+本示例用于在阿里云上使用CDN加速ECS上的静态资源。
+详情可查看[CDN加速ECS资源](https://help.aliyun.com/zh/cdn/use-cases/use-alibaba-cloud-cdn-to-accelerate-the-retrieval-of-resources-from-an-ecs-instance)。
+<!-- DOCS_DESCRIPTION_CN -->
+
+<!-- DOCS_DESCRIPTION_EN -->
+This example is used to use CDN to accelerate the retrieval of resources from an Elastic Compute Service (ECS) instance on Alibaba Cloud.
+More details in [Use Alibaba Cloud CDN to accelerate the retrieval of resources from an ECS instance](https://help.aliyun.com/zh/cdn/use-cases/use-alibaba-cloud-cdn-to-accelerate-the-retrieval-of-resources-from-an-ecs-instance).
+<!-- DOCS_DESCRIPTION_EN -->
+
+<!-- BEGIN_TF_DOCS -->
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_alicloud"></a> [alicloud](#provider\_alicloud) | n/a |
+| <a name="provider_random"></a> [random](#provider\_random) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [alicloud_alidns_record.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/alidns_record) | resource |
+| [alicloud_cdn_domain_new.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/cdn_domain_new) | resource |
+| [alicloud_instance.instance](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/instance) | resource |
+| [alicloud_security_group.group](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/security_group) | resource |
+| [alicloud_security_group_rule.egress](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/security_group_rule) | resource |
+| [alicloud_vpc.vpc](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/vpc) | resource |
+| [alicloud_vswitch.vswitch](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/vswitch) | resource |
+| [random_integer.default](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer) | resource |
+| [alicloud_zones.default](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/data-sources/zones) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_creater_ecs"></a> [creater\_ecs](#input\_creater\_ecs) | Do you want to create a ECS instance | `bool` | `true` | no |
+| <a name="input_domain_home"></a> [domain\_home](#input\_domain\_home) | your domain name | `string` | `"Change to your domain name"` | no |
+| <a name="input_existed_ecs_address"></a> [existed\_ecs\_address](#input\_existed\_ecs\_address) | The address of your existed ecs | `string` | `""` | no |
+| <a name="input_existed_ecs_port"></a> [existed\_ecs\_port](#input\_existed\_ecs\_port) | The port of your existed ecs | `string` | `""` | no |
+| <a name="input_region"></a> [region](#input\_region) | n/a | `string` | `"cn-beijing"` | no |
+<!-- END_TF_DOCS -->
+
+## Documentation
+<!-- docs-link --> 
+
+The template is based on Aliyun document: [Use Alibaba Cloud CDN to accelerate the retrieval of resources from an ECS instance](https://help.aliyun.com/zh/cdn/use-cases/use-alibaba-cloud-cdn-to-accelerate-the-retrieval-of-resources-from-an-ecs-instance) 
+
+<!-- docs-link --> 
---
+++ quickstarts/CDN/201-use-case-cdn-accelerate-resource-from-ecs/main.tf
@@ -0,0 +1,126 @@
+variable "region" {
+  default = "cn-beijing"
+}
+
+# 域名(改为您的域名)
+variable "domain_home" {
+  default     = "Change to your domain name"
+  description = "your domain name"
+}
+
+# 是否创建ECS
+variable "creater_ecs" {
+  default     = true
+  type        = bool
+  description = "Do you want to create a ECS instance"
+}
+
+# ECS地址
+variable "existed_ecs_address" {
+  default     = ""
+  type        = string
+  description = "The address of your existed ecs "
+}
+
+# ECS端口
+variable "existed_ecs_port" {
+  default     = ""
+  type        = string
+  description = "The port of your existed ecs "
+}
+
+locals {
+  available_disk_category = "cloud_essd"
+  vpc_cidr_block          = "172.16.0.0/16"
+  vsw_cidr_block          = "172.16.0.0/24"
+  instance_type           = "ecs.e-c1m1.large"
+  image_id                = "win2022_21H2_x64_dtc_en-us_40G_alibase_20241211.vhd"
+  password                = "Terraform@Example"
+}
+
+provider "alicloud" {
+  region = var.region
+}
+
+data "alicloud_zones" "default" {
+  available_instance_type     = local.instance_type
+  available_resource_creation = "VSwitch"
+  available_disk_category     = local.available_disk_category
+}
+
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
+resource "alicloud_vpc" "vpc" {
+  count      = var.creater_ecs ? 1 : 0
+  vpc_name   = "vpc-test_${random_integer.default.result}"
+  cidr_block = local.vpc_cidr_block
+}
+
+resource "alicloud_security_group" "group" {
+  count               = var.creater_ecs ? 1 : 0
+  security_group_name = "test_${random_integer.default.result}"
+  vpc_id              = alicloud_vpc.vpc.0.id
+}
+
+# 添加允许TCP 80端口出方向流量的规则
+resource "alicloud_security_group_rule" "egress" {
+  count             = var.creater_ecs ? 1 : 0
+  type              = "egress"                           # 出方向规则
+  ip_protocol       = "tcp"                              # TCP协议
+  nic_type          = "intranet"                         # 内网网卡类型（VPC环境）
+  policy            = "accept"                           # 允许策略
+  port_range        = "8/80"                             # 允许80端口
+  priority          = 1                                  # 优先级设置
+  security_group_id = alicloud_security_group.group.0.id # 关联的安全组ID
+  cidr_ip           = "10.0.0.0/8"                       # 允许的IP地址范围，示例为10.0.0.0/8
+}
+
+# 交换机
+resource "alicloud_vswitch" "vswitch" {
+  count        = var.creater_ecs ? 1 : 0
+  vpc_id       = alicloud_vpc.vpc.0.id
+  cidr_block   = local.vsw_cidr_block
+  zone_id      = data.alicloud_zones.default.zones[0].id
+  vswitch_name = "vswitch-test-${random_integer.default.result}"
+}
+
+# ECS实例
+resource "alicloud_instance" "instance" {
+  count                      = var.creater_ecs ? 1 : 0
+  availability_zone          = data.alicloud_zones.default.zones[0].id
+  security_groups            = alicloud_security_group.group.*.id
+  instance_type              = local.instance_type
+  system_disk_category       = "cloud_essd"
+  system_disk_name           = "test_foo_system_disk_${random_integer.default.result}"
+  system_disk_description    = "test_foo_system_disk_description"
+  image_id                   = "aliyun_2_1903_x64_20G_alibase_20240628.vhd"
+  instance_name              = "test_ecs_${random_integer.default.result}"
+  vswitch_id                 = alicloud_vswitch.vswitch.0.id
+  internet_max_bandwidth_out = 10
+  password                   = "Terraform@Example"
+}
+
+# 添加一个加速域名
+resource "alicloud_cdn_domain_new" "example" {
+  domain_name = var.domain_home
+  cdn_type    = "web"
+  scope       = "overseas"
+  sources {
+    content  = var.creater_ecs ? alicloud_instance.instance.0.public_ip : var.existed_ecs_address
+    type     = "ipaddr"
+    priority = "20"
+    port     = 80
+  }
+}
+
+# 域名解析
+resource "alicloud_alidns_record" "example" {
+  domain_name = var.domain_home
+  type        = "CNAME"
+  rr          = "image"
+  value       = alicloud_cdn_domain_new.example.cname
+  ttl         = 600
+}

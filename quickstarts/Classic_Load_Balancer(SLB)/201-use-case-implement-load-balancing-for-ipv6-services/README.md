9d2851d9d8b35a78009bfcb217be202d6810a2bb
lsy <3096027587@qq.com>
2025-01-23 10:26:36 +0800

    201-use-case-implement-load-balancing-for-ipv6-services

---
+++ quickstarts/Classic_Load_Balancer(SLB)/201-use-case-implement-load-balancing-for-ipv6-services/README.md
@@ -0,0 +1,62 @@
+## Introduction
+
+<!-- DOCS_DESCRIPTION_CN -->
+本示例用于在阿里云上为双栈ALB实例开启IPv6挂载，即ALB实例同时支持挂载IPv4和IPv6的云服务器ECS。
+详情可查看[ALB快速实现IPv6服务的负载均衡](https://help.aliyun.com/zh/slb/application-load-balancer/getting-started/implement-load-balancing-for-ipv6-services)。
+<!-- DOCS_DESCRIPTION_CN -->
+
+<!-- DOCS_DESCRIPTION_EN -->
+This example is used to configure a dual-stack server group that contains IPv4 and IPv6 Elastic Compute Service (ECS) instances for a dual-stack ALB instance on Alibaba Cloud.
+More details in [Use ALB to balance loads for IPv6 services](https://help.aliyun.com/zh/slb/application-load-balancer/getting-started/implement-load-balancing-for-ipv6-services).
+<!-- DOCS_DESCRIPTION_EN -->
+
+<!-- BEGIN_TF_DOCS -->
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_alicloud"></a> [alicloud](#provider\_alicloud) | n/a |
+| <a name="provider_random"></a> [random](#provider\_random) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [alicloud_alb_listener.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/alb_listener) | resource |
+| [alicloud_alb_load_balancer.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/alb_load_balancer) | resource |
+| [alicloud_alb_server_group.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/alb_server_group) | resource |
+| [alicloud_dns_record.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/dns_record) | resource |
+| [alicloud_ecs_command.backup_ecs_command](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/ecs_command) | resource |
+| [alicloud_ecs_command.master_ecs_command](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/ecs_command) | resource |
+| [alicloud_ecs_invocation.backup_invocation](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/ecs_invocation) | resource |
+| [alicloud_ecs_invocation.master_invocation](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/ecs_invocation) | resource |
+| [alicloud_instance.backup_example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/instance) | resource |
+| [alicloud_instance.master_example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/instance) | resource |
+| [alicloud_security_group.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/security_group) | resource |
+| [alicloud_security_group_rule.egress](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/security_group_rule) | resource |
+| [alicloud_security_group_rule.ingress](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/security_group_rule) | resource |
+| [alicloud_vpc.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/vpc) | resource |
+| [alicloud_vswitch.backup_vswitch](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/vswitch) | resource |
+| [alicloud_vswitch.master_vswitch](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/resources/vswitch) | resource |
+| [random_integer.example](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer) | resource |
+| [alicloud_zones.example](https://registry.terraform.io/providers/aliyun/alicloud/latest/docs/data-sources/zones) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_host_name"></a> [host\_name](#input\_host\_name) | your domain name | `string` | `"tf-example.com"` | no |
+| <a name="input_password"></a> [password](#input\_password) | ECS登录密码 | `string` | `"Terraform@Example"` | no |
+| <a name="input_region"></a> [region](#input\_region) | n/a | `string` | `"cn-beijing"` | no |
+<!-- END_TF_DOCS -->
+
+## Documentation
+<!-- docs-link --> 
+
+The template is based on Aliyun document: [Use ALB to balance loads for IPv6 services](https://help.aliyun.com/zh/slb/application-load-balancer/getting-started/implement-load-balancing-for-ipv6-services) 
+
+<!-- docs-link --> 
---
+++ quickstarts/Classic_Load_Balancer(SLB)/201-use-case-implement-load-balancing-for-ipv6-services/main.tf
@@ -0,0 +1,281 @@
+variable "region" {
+  default = "cn-beijing"
+}
+
+provider "alicloud" {
+  region = var.region
+}
+
+# 可用区
+data "alicloud_zones" "example" {
+  available_resource_creation      = "VSwitch"
+  available_disk_category          = local.available_disk_category
+  available_slb_address_ip_version = "ipv6"
+  available_slb_address_type       = "classic_internet"
+}
+
+# ECS登录密码
+variable "password" {
+  type    = string
+  default = "Terraform@Example"
+}
+
+# 域名(改为您的域名)
+variable "host_name" {
+  type        = string
+  default     = "tf-example.com"
+  description = "your domain name"
+}
+
+locals {
+  # 专有网络VPC ipv4网段
+  vpc_cidr_ipv4_block = "172.16.0.0/16"
+  # master交换机ipv4网段
+  master_vsw_cidr_ipv4_block = "172.16.0.0/24"
+  # backup交换机ipv4网段
+  backup_vsw_cidr_ipv4_block = "172.16.2.0/24"
+  # ECS规格
+  instance_type = "ecs.e-c1m1.large"
+  # ECS系统镜像
+  image_id                = "aliyun_2_1903_x64_20G_alibase_20240628.vhd"
+  available_disk_category = "cloud_essd"
+  # ECS中部署服务脚本
+  master_ecs_command = <<EOS
+    yum install -y nginx
+    systemctl start nginx.service
+    cd /usr/share/nginx/html/
+    echo "Hello World ! This is ipv6 ECS." > index.html
+    EOS
+  backup_ecs_command = <<EOS
+    yum install -y nginx
+    systemctl start nginx.service
+    cd /usr/share/nginx/html/
+    echo "Hello World ! This is ipv4 ECS." > index.html
+    EOS
+}
+
+# 随机数，取值${random_integer.example.result}
+resource "random_integer" "example" {
+  min = 10000
+  max = 99999
+}
+
+# 专有网络VPC
+resource "alicloud_vpc" "example" {
+  vpc_name    = "vpc_name_${random_integer.example.result}"
+  cidr_block  = local.vpc_cidr_ipv4_block
+  enable_ipv6 = true
+  lifecycle {
+    ignore_changes = [
+      ipv6_cidr_block
+    ]
+  }
+}
+
+# master交换机
+resource "alicloud_vswitch" "master_vswitch" {
+  vpc_id               = alicloud_vpc.example.id
+  enable_ipv6          = true
+  cidr_block           = local.master_vsw_cidr_ipv4_block
+  zone_id              = data.alicloud_zones.example.zones[0].id
+  ipv6_cidr_block_mask = 64
+  vswitch_name         = "master_vswitch_test_${random_integer.example.result}"
+}
+
+# backup交换机
+resource "alicloud_vswitch" "backup_vswitch" {
+  vpc_id               = alicloud_vpc.example.id
+  enable_ipv6          = true
+  cidr_block           = local.backup_vsw_cidr_ipv4_block
+  zone_id              = data.alicloud_zones.example.zones[1].id
+  ipv6_cidr_block_mask = 54
+  vswitch_name         = "backup_vswitch_test_${random_integer.example.result}"
+}
+
+# alb 实例
+resource "alicloud_alb_load_balancer" "example" {
+  load_balancer_edition  = "Basic"
+  load_balancer_name     = "load_balancer_name_${random_integer.example.result}"
+  address_type           = "Internet"
+  address_ip_version     = "DualStack"
+  address_allocated_mode = "Fixed"
+  vpc_id                 = alicloud_vpc.example.id
+  load_balancer_billing_config {
+    pay_type = "PayAsYouGo"
+  }
+  zone_mappings {
+    vswitch_id = alicloud_vswitch.master_vswitch.id
+    zone_id    = alicloud_vswitch.master_vswitch.zone_id
+  }
+  zone_mappings {
+    vswitch_id = alicloud_vswitch.backup_vswitch.id
+    zone_id    = alicloud_vswitch.backup_vswitch.zone_id
+  }
+}
+
+# 安全组
+resource "alicloud_security_group" "example" {
+  security_group_name = "security_group_name_${random_integer.example.result}"
+  vpc_id              = alicloud_vpc.example.id
+}
+
+# 添加允许TCP 80端口入方向流量的规则
+resource "alicloud_security_group_rule" "ingress" {
+  type              = "ingress"                          # 入方向规则
+  ip_protocol       = "tcp"                              # TCP协议
+  nic_type          = "intranet"                         # 内网网卡类型（VPC环境）
+  policy            = "accept"                           # 允许策略
+  port_range        = "80/80"                            # 允许80端口
+  priority          = 1                                  # 优先级设置
+  security_group_id = alicloud_security_group.example.id # 关联的安全组ID
+  cidr_ip           = "10.0.0.0/8"                       # 允许的IP地址范围，示例为10.0.0.0/8
+}
+
+# 添加允许TCP 80端口出方向流量的规则
+resource "alicloud_security_group_rule" "egress" {
+  type              = "egress"                           # 入方向规则
+  ip_protocol       = "tcp"                              # TCP协议
+  nic_type          = "intranet"                         # 内网网卡类型（VPC环境）
+  policy            = "accept"                           # 允许策略
+  port_range        = "80/80"                            # 允许80端口
+  priority          = 1                                  # 优先级设置
+  security_group_id = alicloud_security_group.example.id # 关联的安全组ID
+  cidr_ip           = "10.0.0.0/8"                       # 允许的IP地址范围，示例为10.0.0.0/8
+}
+
+# mster ECS实例
+resource "alicloud_instance" "master_example" {
+  availability_zone          = data.alicloud_zones.example.zones[0].id
+  security_groups            = alicloud_security_group.example.*.id
+  instance_type              = local.instance_type
+  system_disk_category       = local.available_disk_category
+  system_disk_name           = "master_system_disk_name_${random_integer.example.result}"
+  system_disk_description    = "master_system_disk_description_${random_integer.example.result}"
+  image_id                   = local.image_id
+  instance_name              = "master_instance_name_${random_integer.example.result}"
+  vswitch_id                 = alicloud_vswitch.master_vswitch.id
+  internet_max_bandwidth_out = 10
+  password                   = var.password
+  ipv6_address_count         = 1
+}
+
+# backup ECS实例
+resource "alicloud_instance" "backup_example" {
+  availability_zone          = data.alicloud_zones.example.zones[1].id
+  security_groups            = alicloud_security_group.example.*.id
+  instance_type              = local.instance_type
+  system_disk_category       = local.available_disk_category
+  system_disk_name           = "backup_system_disk_name_${random_integer.example.result}"
+  system_disk_description    = "backup_system_disk_description_${random_integer.example.result}"
+  image_id                   = local.image_id
+  instance_name              = "backup_instance_name_${random_integer.example.result}"
+  vswitch_id                 = alicloud_vswitch.backup_vswitch.id
+  internet_max_bandwidth_out = 10
+  password                   = var.password
+}
+
+# master ECS命令
+resource "alicloud_ecs_command" "master_ecs_command" {
+  name             = "master_ecs_command_name"
+  description      = "master_ecs_command_description"
+  enable_parameter = false
+  type             = "RunShellScript"
+  command_content  = base64encode(local.master_ecs_command)
+  timeout          = 3600
+  working_dir      = "/root"
+}
+
+# 在master ECS中执行命令
+resource "alicloud_ecs_invocation" "master_invocation" {
+  instance_id = [alicloud_instance.master_example.id]
+  command_id  = alicloud_ecs_command.master_ecs_command.id
+  timeouts {
+    create = "5m"
+  }
+}
+
+# backup ECS命令
+resource "alicloud_ecs_command" "backup_ecs_command" {
+  name             = "backup_ecs_command_name"
+  description      = "backup_ecs_command_description"
+  enable_parameter = false
+  type             = "RunShellScript"
+  command_content  = base64encode(local.backup_ecs_command)
+  timeout          = 3600
+  working_dir      = "/root"
+}
+
+# 在backup ECS中执行命令
+resource "alicloud_ecs_invocation" "backup_invocation" {
+  instance_id = [alicloud_instance.backup_example.id]
+  command_id  = alicloud_ecs_command.backup_ecs_command.id
+  timeouts {
+    create = "5m"
+  }
+}
+
+# alb服务器组
+resource "alicloud_alb_server_group" "example" {
+  protocol          = "HTTP"
+  server_group_name = "server_group_name_${random_integer.example.result}"
+  vpc_id            = alicloud_vpc.example.id
+
+  servers {
+    port        = 80
+    server_id   = alicloud_instance.master_example.id
+    server_ip   = alicloud_instance.master_example.private_ip
+    server_type = "Ecs"
+  }
+
+  servers {
+    port        = 80
+    server_id   = alicloud_instance.backup_example.id
+    server_ip   = alicloud_instance.backup_example.private_ip
+    server_type = "Ecs"
+  }
+
+  sticky_session_config {
+    sticky_session_enabled = true
+    cookie                 = "cookie_${random_integer.example.result}"
+    sticky_session_type    = "Server"
+  }
+
+  health_check_config {
+    health_check_connect_port = "80"
+    health_check_enabled      = true
+    health_check_host         = var.host_name
+    health_check_codes        = ["http_2xx", "http_3xx", "http_4xx"]
+    health_check_http_version = "HTTP1.1"
+    health_check_interval     = "2"
+    health_check_method       = "HEAD"
+    health_check_path         = "/tf-example"
+    health_check_protocol     = "HTTP"
+    health_check_timeout      = 5
+    healthy_threshold         = 3
+    unhealthy_threshold       = 3
+  }
+}
+
+# alb监听
+resource "alicloud_alb_listener" "example" {
+  listener_protocol = "HTTP"
+  listener_port     = 80
+  load_balancer_id  = alicloud_alb_load_balancer.example.id
+  default_actions {
+    type = "ForwardGroup"
+    forward_group_config {
+      server_group_tuples {
+        server_group_id = alicloud_alb_server_group.example.id
+      }
+    }
+  }
+}
+
+# 域名解析
+resource "alicloud_dns_record" "example" {
+  name        = var.host_name
+  type        = "CNAME"
+  host_record = "@"
+  value       = alicloud_alb_load_balancer.example.dns_name
+  ttl         = 600
+}

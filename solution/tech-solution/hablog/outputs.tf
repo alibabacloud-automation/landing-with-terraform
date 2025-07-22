ea799adfdc174f415536038ee9a4271825d77338
翼鹏 <yipeng.zpf@alibaba-inc.com>
2025-07-22 11:26:23 +0800

    feat: 解决方案basic-highly-available-architecture tf文件完成

---
+++ solution/tech-solution/hablog/outputs.tf
@@ -0,0 +1,9 @@
+output "WebUrl" {
+  description = <<EOT
+    {
+      "zh-cn": "Web 访问地址。",
+      "en": "The Addresses of Web."
+    }
+    EOT
+  value = format("http://%s", alicloud_alb_load_balancer.alb.dns_name)
+}
